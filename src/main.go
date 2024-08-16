fbfd39035e3ded4956ea9907ef6a3484fc136030
Jonathan Herrmann <jonorh@web.de>
2024-08-16 22:19:07 +0200

    Connect mongodb

--- src/main.go
+++ src/main.go
@@ -1,8 +1,50 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	DATABASE                = "gems"
+	LATIN_PHRASE_COLLECTION = "latin-phrases"
+)
+
+func closeMongoConnection(client *mongo.Client) {
+	if err := client.Disconnect(context.Background()); err != nil {
+		panic(err)
+	}
+}
+
+func ensureMongoDbIsSetUp(client *mongo.Client) {
+	//database := client.Database(DATABASE)
+
+	// create indicies
+}
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable. See: www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer closeMongoConnection(client)
+	ensureMongoDbIsSetUp(client)
+
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
